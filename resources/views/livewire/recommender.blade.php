f9c0405951471ba046e70aafc7d5b841d49e8203
XirTech <john.darlucio022@gmail.com>
2025-01-27 15:10:52 +0800

    adjust UI

--- resources/views/livewire/recommender.blade.php
+++ resources/views/livewire/recommender.blade.php
@@ -8,90 +8,91 @@
                     <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">
                         The Course Recommender System is exclusively developed for ISAT U Miagao Campus which aims to
                         recommend courses for the incoming college students.</p>
-                    <form action="">
-                        <div class="dark:bg-gray-800 rounded-xl mb-4">
-                            <div class="text-start">
-                                <p class="p-4 dark:text-white uppercase text-md">Student Information</p>
-                                <hr class="w-full bg-gray-100 border-1 rounded-sm dark:bg-gray-300">
-                            </div>
-                            <section class="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 p-8">
-                                <!-- Full Name -->
-                                <div class="text-start">
-                                    <label for="name"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Full Name <span class="text-red-500">*</span>
-                                    </label>
-                                    <input type="text" id="name"
-                                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                           placeholder="John Doe" required>
-                                </div>
-
-                                <!-- Average -->
-                                <div class="text-start">
-                                    <label for="average"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Average <span class="text-red-500">*</span>
-                                    </label>
-                                    <input type="number" id="average" min="75" max="100" value="75"
-                                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                           placeholder="Enter your Final Average" required>
-                                </div>
-
-                                <!-- School -->
-                                <div class="text-start sm:col-span-2">
-                                    <label for="school"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        School <span class="text-red-500">*</span>
-                                    </label>
-                                    <input type="text" id="school"
-                                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                           placeholder="Enter your School Here" required>
-                                </div>
-                            </section>
-
-                        </div>
-                        <div class="dark:bg-gray-800 rounded-xl">
-                            <div class="text-start">
-                                <p class="p-4 dark:text-white uppercase text-md">List of Skills</p>
-                                <hr class="w-full bg-gray-100 border-1 rounded-sm dark:bg-gray-300">
-                            </div>
-                            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8">
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                                <div class="flex items-center mb-2">
-                                    <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+                <form action="" class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    <div class="dark:bg-gray-800 rounded-xl">
+                        <div class="text-start">
+                            <p class="p-4 dark:text-white uppercase text-md">Student Information</p>
+                            <hr class="w-full bg-gray-100 border-1 rounded-sm dark:bg-gray-300">
+                        </div>
+                        <section class="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 p-8">
+                            <!-- Full Name -->
+                            <div class="text-start">
+                                <label for="name"
+                                       class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                    Full Name <span class="text-red-500">*</span>
+                                </label>
+                                <input type="text" id="name"
+                                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                       placeholder="John Doe" required>
+                            </div>
+
+                            <!-- Average -->
+                            <div class="text-start">
+                                <label for="average"
+                                       class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                    Average <span class="text-red-500">*</span>
+                                </label>
+                                <input type="number" id="average" min="75" max="100" value="75"
+                                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                       placeholder="Enter your Final Average" required>
+                            </div>
+
+                            <!-- School -->
+                            <div class="text-start sm:col-span-2">
+                                <label for="school"
+                                       class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                    School <span class="text-red-500">*</span>
+                                </label>
+                                <input type="text" id="school"
+                                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                       placeholder="Enter your School Here" required>
+                            </div>
+                        </section>
+
+                    </div>
+                    <div class="dark:bg-gray-800 rounded-xl">
+                        <div class="text-start">
+                            <p class="p-4 dark:text-white uppercase text-md">List of Skills</p>
+                            <hr class="w-full bg-gray-100 border-1 rounded-sm dark:bg-gray-300">
+                        </div>
+                        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8">
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                            <div class="flex items-center mb-2">
+                                <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default checkbox</label>
+                            </div>
+                        </div>
+                    </div>
+                <button type="submit" class="w-full md:w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+                </form>
             </div>
         </section>
     </div>
